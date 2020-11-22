026d1a305e171b10d6bf403a62a1ec83e4f3120a
Martin Garcia Blanco <martin-garcia-blanco@MacBook-Pro.local>
2020-11-22 12:20:35 +0100

    eslint-staged, prettier and eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,12 @@
 module.exports = {
   extends: [
-    "plugin:react/recommended",
-    "plugin:prettier/recommended",
-    "prettier/react",
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
   ],
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   env: {
     browser: true,
   },
-  rules: {
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    // "comma-dangle": ["warn", "never"],
-    "prefer-template": 1,
-    "import/prefer-default-export": 0,
-    "react/jsx-props-no-spreading": [
-      "error",
-      {
-        html: "ignore",
-        custom: "ignore",
-        exceptions: [""],
-      },
-    ],
-  },
-  plugins: ["prettier", "react"],
+  plugins: ['prettier', 'react'],
 };
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 const App = () => {
   return <h1>My App</h1>;
